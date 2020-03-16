566ccfc0fe718b539cb8d7008629894ed9dc365b
iovefish <46422017+iovefish@users.noreply.github.com>
2020-03-16 15:32:03 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -5,5 +5,6 @@
 # Author: P3TERX
 # Blog: https://p3terx.com
 #=================================================
-# Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+git clone https://github.com/coolsnowwolf/lede
+Modify default IP
+sed -i 's/192.168.1.1/10.10.10.5/g' package/base-files/files/bin/config_generate
